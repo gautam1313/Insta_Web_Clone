b6efe91eb6886434124016c2cdf266d3ac16a3a8
Sebby80 <sebkm80@gmail.com>
2021-10-01 13:51:25 +0530

    Adding login functionality

--- src/App.js
+++ src/App.js
@@ -2,6 +2,7 @@ import { lazy, Suspense } from "react";
 import { BrowserRouter as Router, Route, Switch } from "react-router-dom";
 import * as ROUTES from "./constants/routes";
 const Login = lazy(() => import("./pages/login"));
+const Signup = lazy(() => import("./pages/signup"));
 
 function App() {
   return (
@@ -9,6 +10,7 @@ function App() {
       <Suspense fallback={<p>Loading...</p>}>
         <Switch>
           <Route path={ROUTES.LOGIN} component={Login} />
+          <Route path={ROUTES.SIGN_UP} component={Signup} />
         </Switch>
       </Suspense>
     </Router>
--- src/pages/login.js
+++ src/pages/login.js
@@ -1,6 +1,7 @@
 import React, { useState, useContext, useEffect } from "react";
-import { useHistory } from "react-router-dom";
+import { Link, useHistory } from "react-router-dom";
 import FirebaseContext from "../context/firebase";
+import * as ROUTES from "../constants/routes";
 
 const login = () => {
   const history = useHistory();
@@ -14,7 +15,17 @@ const login = () => {
   const invalid = emailAddress === "" || password === "";
 
   //On submit
-  const handleLogin = () => {};
+  const handleLogin = async (event) => {
+    event.preventDefault();
+    try {
+      await firebase.auth().signInWithEmailAndPassword(emailAddress, password);
+      history.replace(ROUTES.DASHBOARD);
+    } catch (error) {
+      setEmailAddress("");
+      setPassword("");
+      setError(error.message);
+    }
+  };
 
   useEffect(() => {
     document.title = "Login - Instagram";
@@ -26,7 +37,7 @@ const login = () => {
         <img src="/images/iphone-with-profile.jpg" alt="iphone Instagram" />
       </div>
       <div className="w-2/5 flex flex-col">
-        <div className="bg-white border border-gray-500 border-gray-primary mb-4 p-4">
+        <div className="bg-white border border-gray-primary mb-4 p-4 rounded">
           <h1 className="flex w-full justify-center">
             <img
               src="/images/logo.png"
@@ -35,16 +46,14 @@ const login = () => {
             />
           </h1>
 
-          {error && (
-            <p className="text-red-500 text-red-primary mb-4">{error}</p>
-          )}
+          {error && <p className="text-red-primary mb-4">{error}</p>}
 
           <form onSubmit={handleLogin} method="POST">
             <input
               type="text"
               aria-label="Enter email address"
               placeholder="Email"
-              className="text-gray-500 text-gray-base w-full mr-3 py-5 px-4 h-2 text-sm border border-gray-primary border-gray-500 rounded mb-2"
+              className="text-gray-base w-full mr-3 py-5 px-4 h-2 text-sm border border-gray-primary rounded mb-2"
               onChange={({ target }) => {
                 setEmailAddress(target.value);
               }}
@@ -53,7 +62,7 @@ const login = () => {
               type="password"
               aria-label="Enter password"
               placeholder="Password"
-              className="text-gray-500 text-gray-base w-full mr-3 py-5 px-4 h-2 text-sm border border-gray-primary border-gray-500 rounded mb-2"
+              className="text-gray-base w-full mr-3 py-5 px-4 h-2 text-sm border border-gray-primary rounded mb-2"
               onChange={({ target }) => {
                 setPassword(target.value);
               }}
@@ -61,7 +70,7 @@ const login = () => {
             <button
               disabled={invalid}
               type="submit"
-              className={`bg-blue-500 w-full rounded h-8 font-bold text-white ${
+              className={`bg-blue-medium w-full rounded h-8 font-bold text-white ${
                 invalid && "opacity-50"
               }`}
             >
@@ -69,8 +78,13 @@ const login = () => {
             </button>
           </form>
         </div>
-        <div className="bg-white w-full flex flex-col justify-center items-center p-4 border border-gray-500 border-gray-primary">
-          <p className="text-sm">Don't have an account?</p>
+        <div className="bg-white w-full flex flex-col justify-center items-center p-4 border border-gray-primary rounded">
+          <p className="text-sm">
+            Don't have an account?&nbsp;{" "}
+            <Link to={ROUTES.SIGN_UP} className="font-bold text-blue-medium">
+              Sign Up
+            </Link>
+          </p>
         </div>
       </div>
     </div>

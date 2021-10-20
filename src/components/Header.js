ee118431f20b1d26e97a97f3efc6d000d3e4bf9f
Sebby80 <sebkm80@gmail.com>
2021-10-20 11:38:44 +0530

    Redirecting to login on signout

--- src/components/Header.js
+++ src/components/Header.js
@@ -1,5 +1,5 @@
 import { useContext } from "react";
-import { Link } from "react-router-dom";
+import { Link, useHistory } from "react-router-dom";
 import FirebaseContext from "../context/firebase";
 import UserContext from "../context/user";
 import * as ROUTES from "../constants/routes";
@@ -7,6 +7,7 @@ import * as ROUTES from "../constants/routes";
 const Header = () => {
   const { auth, signOut } = useContext(FirebaseContext);
   const user = useContext(UserContext);
+  const history = useHistory();
 
   return (
     <header className="h-16 bg-white border-b border-gray-primary mb-8">
@@ -45,10 +46,14 @@ const Header = () => {
                 <button
                   type="button"
                   title="Sign Out"
-                  onClick={() => signOut(auth)}
+                  onClick={() => {
+                    signOut(auth);
+                    history.push(ROUTES.LOGIN);
+                  }}
                   onKeyDown={(e) => {
                     if (e.key === "Enter") {
                       signOut(auth);
+                      history.push(ROUTES.LOGIN);
                     }
                   }}
                 >
